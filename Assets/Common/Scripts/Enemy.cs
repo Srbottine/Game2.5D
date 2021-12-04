136cc948e9fc70f026887a3832a296528d524333
Srbottine <lucasbottine7@gmail.com>
2021-12-03 22:40:55 -0300

    finalizado IA enimigo

--- Assets/Common/Scripts/Enemy.cs
+++ Assets/Common/Scripts/Enemy.cs
@@ -1,44 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
-namespace Common.Scripts
+
+public class Enemy : MonoBehaviour
 {
-    public class Enemy : MonoBehaviour
+  public float speed;
+  public float checkRadius;
+  public float attackRadius;
+
+  public bool shouldRotate;
+
+  public LayerMask whatIsPlayer;
+
+   private Transform target;
+   private Rigidbody2D rb;
+   private Animator anim;
+   private Vector2 movement;
+   public Vector3 dir;
+
+   private bool isInChaseRange;
+   private bool isInAttackRange;
+
+   private void Start()
+   {
+     rb = GetComponent<Rigidbody2D>();
+     anim = GetComponent<Animator>();
+    target = GameObject. FindWithTag("Player").transform;
+   }
+   private void Update()
+   {
+     anim.SetBool("isRunning", isInChaseRange);
+
+        isInChaseRange = Physics2D.OverlapCircle(transform.position, checkRadius, whatIsPlayer);
+        isInAttackRange = Physics2D.OverlapCircle(transform.position, attackRadius, whatIsPlayer);
+
+     dir = target.position - transform.position;
+     float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+     dir.Normalize();
+     movement = dir;
+    if(shouldRotate)
     {
-
-    #region Constants
-
-
-    #endregion
-
-    #region Inspector
-
-
-    [Header("Relations")]
-    [SerializeField]
-    private Animator animator = null;
-
-    [SerializeField]
-    private Rigidbody physicsBody = null;
-
-    [SerializeField]
-    private SpriteRenderer spriteRenderer = null;
-
-    #endregion
-
-     #region Fields
-
-    private Vector3 _movement;
-
-    #endregion
-
-
-    #region MonoBehaviour
-
-
-
-    #endregion
+     anim.SetFloat("X", dir.x);
+     anim.SetFloat("V", dir.y);
     }
-
+}
+private void Fixedupdate()
+{
+    if(isInChaseRange && !isInAttackRange)
+    {
+        MoveCharacter(movement);
+    }
+        if(isInAttackRange)
+    {
+        rb.velocity = Vector2.zero;
+    }
+}
+private void MoveCharacter(Vector2 dir)
+    {
+        rb. MovePosition((Vector2)transform.position + (dir * speed * Time.deltaTime));
+    }
 }
